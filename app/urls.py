55169caddefe74ad612f3e401e1fc57b45483e85
seregaalhimenko <seregaalhimenko@gmail.com>
2022-04-17 03:34:52 +0700

    moving the result handler and test start from /start(result)-app/<pk_test> to /<pk_test>/start(result)

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path, include, re_path
-# from app import api
+from django.urls import path, include
 from app.routers import routers
-from app.views import test, question, answer_tracker, choice, views, result_test
 
 
 urlpatterns = [
     path('', include(routers.router.urls)),
-    path('start-app/<int:test_id>/',views.StartQuiz.as_view(), name="start_app"),
-    path('result/<int:test_id>/', result_test.Result.as_view(), name="result_test"),
-
-
 ]
--- app/views/test.py
+++ app/views/test.py
@@ -1,10 +1,28 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import permissions
+
 from app.models import Test
 from app.serializers import TestSerializer as Serializer
 from app.castom_permission import AuthorOrReadOnly
-from rest_framework import viewsets
+from app.source import start_quiz
+from app.result_serializers import ResultSerializer
+
 
 class TestViewSet(viewsets.ModelViewSet):
     queryset = Test.objects.all()
     serializer_class = Serializer
     permission_classes = [AuthorOrReadOnly]
     tags = ["Test"]
+
+    @action(methods=['post'], detail=True, permission_classes=[permissions.IsAuthenticated],
+            url_path='start', url_name='start_test')
+    def start_quiz(self, request, pk, *args,**kwargs):
+        return start_quiz(request=request,pk=pk)
+
+    @action(methods=['get'], detail=True, permission_classes=[permissions.IsAuthenticated],url_path='result', url_name='result_test')
+    def result(self, request, pk, *args,**kwargs):
+        instance = self.get_object()
+        serializer_obj = ResultSerializer(instance)
+        return Response(data=serializer_obj.data)
